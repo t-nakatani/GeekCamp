31d45b2f31627e1ff34e7e963742cd44d2066791
t-nakatani <tomoya.12@icloud.com>
2023-05-28 17:41:30 +0900

    [ADD] initial commit in history_chrome_extension

---
+++ history_chrome_extension/src/pages/Popup/Popup.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import logo from '../../assets/img/logo.svg';
+import Greetings from '../../containers/Greetings/Greetings';
+import './Popup.css';
+
+const Popup = () => {
+  const handleSearchHistory = () => {
+    chrome.history.search({ text: '', startTime: 0, maxResults: 200 }, function(data) {
+      let urls = data.map(page => page.url); // URLのリストを取得
+      let titles = data.map(page => page.title); // URLのリストを取得
+      console.log('URLs:', urls); // URLのリストをコンソールに出力
+
+      let url = 'http://127.0.0.1:8000/fetch_history/';
+      let data_ = { urls: urls , titles: titles}; // リクエストのデータとしてURLのリストを使用
+      
+      postData(url, data_);
+    });
+  };
+
+const postData = (url, data) => {
+  fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    })
+    .then(data => {
+      console.log('Success:', data);
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
+};
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <button id='searchButton' onClick={handleSearchHistory}>Upload History</button>
+      </header>
+    </div>
+  );
+};
+
+export default Popup;
